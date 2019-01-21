9f91e433880d86a697409eee3e1c9591a5c3c2c3
Christian Hoffmeister <mail@choffmeister.de>
2019-01-21 13:59:43 +0100

    fixup! [skip ci]

--- src/main/scala/de/kddc/mybench/Application.scala
+++ src/main/scala/de/kddc/mybench/Application.scala
@@ -1,37 +1,55 @@
 package de.kddc.mybench
 
+import java.security.MessageDigest
+
 import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source, StreamConverters}
+import akka.util.ByteString
 import com.softwaremill.macwire._
+
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.util.{Failure, Success}
 
 object Application {
   def main(args: Array[String]): Unit = {
     implicit val actorSystem = ActorSystem("mybench")
+    implicit val executor = ExecutionContext.Implicits.global
+    implicit val materializer = ActorMaterializer()
     val service = wire[Service]
     service.start()
+
+    val bytes = Source(0L until 1024L * 1024L * 1024L)
+      .map(i => (i % 256).toByte)
+      .grouped(1024)
+      .map(bs => ByteString(bs.toArray))
+
+    bytes
+      .viaMat(hashFlow("SHA-1"))(Keep.right)
+      .map { chunk =>
+        println(s"chunk $chunk")
+        chunk
+      }
+      .mapMaterializedValue { hashF =>
+        hashF.onComplete {
+          case Success(hash) => println(s"hash $hash")
+          case Failure(error) => println(s"error $error")
+        }
+      }
+      .runWith(Sink.ignore)
+
+    def hashFlow(algorithm: String): Flow[ByteString, ByteString, Future[ByteString]] = {
+      val digest = MessageDigest.getInstance(algorithm)
+      Flow[ByteString]
+        .map { chunk =>
+          digest.update(chunk.toArray)
+          chunk
+        }
+        .watchTermination() { (_, done) =>
+          done.map { _ =>
+            ByteString(digest.digest())
+          }
+        }
+    }
   }
-}
-
-
-
-trait User {
-  val id: Long
-}
-case class AdminUser(id: Long) extends User
-case class RegularUser(id: Long) extends User
-
-object Foobar {
-
-
-
-  val admins = new MyList[AdminUser]()
-  val regularUsers = new MyList[RegularUser]()
-
-  val users: MyList[User] = admins
-
-  users.addItem(AdminUser(1))
-}
-
-class MyList[+A] {
-  def addItem[B >: A](item: B): Unit = ???
-  def elementAt(index: Int): A = ???
 }
