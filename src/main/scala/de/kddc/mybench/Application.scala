4ee251fc66a75f21a3e5ea6212637812663c16d1
Christian Hoffmeister <mail@choffmeister.de>
2019-01-21 14:41:57 +0100

    fixup! [skip ci]

--- src/main/scala/de/kddc/mybench/Application.scala
+++ src/main/scala/de/kddc/mybench/Application.scala
@@ -2,16 +2,76 @@ package de.kddc.mybench
 
 import java.security.MessageDigest
 
-import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
+import akka.actor.{Actor, ActorSystem, Cancellable, Props}
+import akka.stream.{ActorMaterializer, ThrottleMode}
 import akka.stream.scaladsl.{Flow, Keep, Sink, Source, StreamConverters}
 import akka.util.ByteString
 import com.softwaremill.macwire._
 
 import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 
+object MailSender {
+  private case object Tick
+  case object Init
+  case object Ack
+  case object Complete
+  case class Failed(err: Throwable)
+}
+
+class MailSender extends Actor {
+  import MailSender._
+
+  var queue = List.empty[Mail]
+  var schedule = Option.empty[Cancellable]
+  override def receive: Receive = {
+    case mail @ Mail(recipient, subject, body) =>
+      println(s"ENQUEUED MAIL $mail")
+      queue = queue :+ mail
+      self ! Tick
+      sender() ! Ack
+
+    case Tick =>
+      println("TICK")
+      if (queue.nonEmpty) {
+        val mail = queue.head
+        queue = queue.tail
+        if (Math.random() <= 0.5) {
+          println(s"SENT MAIL $mail")
+          self ! Tick
+        } else {
+          println(s"FAILURE")
+          queue = queue :+ mail
+          scheduleTick()
+        }
+      } else {
+        scheduleTick()
+      }
+
+    case Init =>
+      sender() ! Ack
+
+    case Complete =>
+      println(s"complete")
+
+    case msg =>
+      println(s"unexpected $msg")
+  }
+
+  private def scheduleTick(): Unit = {
+    schedule.foreach(_.cancel())
+    schedule = Some(context.system.scheduler.scheduleOnce(1.second, self, Tick)(context.dispatcher))
+  }
+}
+
+
+
+case class Mail(recipient: String, subject: String, body: String)
+
 object Application {
+  import MailSender._
+
   def main(args: Array[String]): Unit = {
     implicit val actorSystem = ActorSystem("mybench")
     implicit val executor = ExecutionContext.Implicits.global
@@ -19,50 +79,13 @@ object Application {
     val service = wire[Service]
     service.start()
 
-    val bytes = Source(0 until 10 * 1024)
-      .map(i => (i % 256).toByte)
-      .grouped(1024)
-      .map(bs => ByteString(bs.toArray))
+    val sender = actorSystem.actorOf(Props[MailSender])
+    val mailgunQueue = Source(1 to 2)
+      .throttle(1, 1.second, 1, ThrottleMode.shaping)
+      .map(i => Mail(s"user$i@domain.com", s"Mail $i", ""))
+      .to(Sink.actorRefWithAck(sender, Init, Ack, Complete, err => Failed(err)))
+      .run()
 
-    bytes
-      .viaMat(hashFlow("SHA-1"))(Keep.right)
-      .viaMat(countFlow())((l, r) => l.zip(r))
-      .map { chunk =>
-        println(s"chunk $chunk")
-        chunk
-      }
-      .mapMaterializedValue { hashCountF =>
-        hashCountF.onComplete {
-          case Success(hash) => println(s"hash and count $hash")
-          case Failure(error) => println(s"error $error")
-        }
-      }
-      .runWith(Sink.ignore)
 
-    def countFlow[T](): Flow[T, T, Future[Long]] = {
-      var counter = 0L
-      Flow[T]
-        .map { elem =>
-          counter = counter + 1
-          elem
-        }
-        .watchTermination() { (_, done) =>
-          done.map(_ => counter)
-        }
-    }
-
-    def hashFlow(algorithm: String): Flow[ByteString, ByteString, Future[ByteString]] = {
-      val digest = MessageDigest.getInstance(algorithm)
-      Flow[ByteString]
-        .map { chunk =>
-          digest.update(chunk.toArray)
-          chunk
-        }
-        .watchTermination() { (_, done) =>
-          done.map { _ =>
-            ByteString(digest.digest())
-          }
-        }
-    }
   }
 }
