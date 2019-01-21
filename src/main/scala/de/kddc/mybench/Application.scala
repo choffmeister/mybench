92f6d880124fcd4c97e692b540ad9c79c1255204
Christian Hoffmeister <mail@choffmeister.de>
2019-01-21 13:30:26 +0100

    fixup! [skip ci]

--- src/main/scala/de/kddc/mybench/Application.scala
+++ src/main/scala/de/kddc/mybench/Application.scala
@@ -10,3 +10,28 @@ object Application {
     service.start()
   }
 }
+
+
+
+trait User {
+  val id: Long
+}
+case class AdminUser(id: Long) extends User
+case class RegularUser(id: Long) extends User
+
+object Foobar {
+
+
+
+  val admins = new MyList[AdminUser]()
+  val regularUsers = new MyList[RegularUser]()
+
+  val users: MyList[User] = admins
+
+  users.addItem(AdminUser(1))
+}
+
+class MyList[+A] {
+  def addItem[B >: A](item: B): Unit = ???
+  def elementAt(index: Int): A = ???
+}
